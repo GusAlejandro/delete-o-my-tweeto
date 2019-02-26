7df5980bce421cf27f8e11acd785462a5508f0cd
Gustavo Cornejo <gcornejo85@gmail.com>
2019-02-26 00:25:56 -0800

    addded photo download functionality

--- extract_media.py
+++ extract_media.py
@@ -1,32 +1,29 @@
 """
-This module handles downloading twitter videos using the tweet id
+This module handles downloading videos and photos attached to tweets
 """
-import sys
 import json
 import uuid
-import requests
 import os
+import requests
 from bs4 import BeautifulSoup
 
 
-INPUT_TWEET = sys.argv[1]
-
-# TODO: refactor this module to handle all twitter media downloads, not just video
+# INPUT_TWEET = sys.argv[1]
 
 def download_video(m3u8_text: str, tweet_id: str):
     """
     Handles storing all ts chunks in the m3u8 text
-    :param m3u8_text: root m3u8 file as text, has urls for all video chunks
-    :param tweet_id: reference id for tweet
+    :param m3u8_text:
+    :param tweet_id:
     :return: None
     """
     video_chunks = ''
     ts_prefix = "https://video.twimg.com"
     for line in m3u8_text.splitlines():
         if "/ext_tw_video/" in line:
-            r = requests.get(ts_prefix + line)
+            req = requests.get(ts_prefix + line)
             chunk_name = str(uuid.uuid4())
-            open(chunk_name + '.ts', 'wb').write(r.content)
+            open(chunk_name + '.ts', 'wb').write(req.content)
             video_chunks += chunk_name +'.ts' + ' '
     file_name = tweet_id + '.ts'
     os.system('cat ' + video_chunks + '> ' + file_name)
@@ -37,21 +34,23 @@ def download_video(m3u8_text: str, tweet_id: str):
 
 
 def get_root_m3u8_url(tweet_id: str, token: str):
+    """
+    GET request on the tweet config endpoint
+    :param tweet_id:
+    :param token:
+    :return: root m3u8 url
+    """
     config_url = 'https://api.twitter.com/1.1/videos/tweet/config/' + tweet_id + '.json'
     config_response = requests.get(config_url, headers={'Authorization': token})
-    if config_response.status_code  == 429:
+    if config_response.status_code == 429:
         return None
-    else:
-        return json.loads(config_response.text)['track']['playbackUrl']
+    return json.loads(config_response.text)['track']['playbackUrl']
 
 
-
-
-
-def main(tweet_id: str):
+def video_main(tweet_id: str):
     """
-    Main func.
-    :param tweet_id: reference id for tweet
+    Main func for video.
+    :param tweet_id:
     :return: None
     """
     # get js file embedded in video html to get bearer token needed to hit the config endpoint for the tweet
@@ -90,4 +89,41 @@ def main(tweet_id: str):
     download_video(video_chunks, tweet_id)
 
 
-main(INPUT_TWEET)
+def download_photo(image_url: str, tweet_id: str):
+    """
+    Downloads the image at the image_url specified in the parameters
+    :param image_url:
+    :param tweet_id:
+    :return:
+    """
+    req = requests.get(image_url)
+    file_name = str(uuid.uuid4())
+    open(tweet_id + '/' + file_name + '.jpg', 'wb').write(req.content)
+
+
+def photo_main(tweet_id: str, username: str):
+    """
+    Main func for photo.
+    :param tweet_id:
+    :param username:
+    :return: None
+    """
+    os.mkdir(tweet_id)
+    tweet_url = 'https://twitter.com/' + username + '/status/' + tweet_id
+    tweet_html = requests.get(tweet_url).text
+
+    # in order to handle tweet thread scenario, uses two layers of beautifulsoup
+    tweet_soup = BeautifulSoup(tweet_html, 'html.parser')
+    top_level_tag = str(tweet_soup.find_all("div", {'data-tweet-id': tweet_id}))
+
+    # identifies images attached to tweet, not including avi's
+    image_container_soup = BeautifulSoup(top_level_tag, 'html.parser')
+    image_list = image_container_soup.find_all("img")
+    for image in image_list:
+        if 'data-aria-label-part' in str(image):
+            download_photo(image['src'], tweet_id)
+
+
+
+# video_main(INPUT_TWEET)
+photo_main('1095158234128601089', 'GusAlejandro_')
